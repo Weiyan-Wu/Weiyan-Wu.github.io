fd50ffe38e8fc226fbbaf69ca5371bdb2c0b9a10
Weiyan Wu <ww333@cornell.edu>
2021-11-11 03:00:40 -0500

    finish

---
+++ scripts/carousel.js
@@ -0,0 +1,54 @@
+const SLIDES = $(".slide");
+
+
+function nextSlide() {
+  let nextNum = SLIDES.index($(".slide:not(.hidden)")) + 1 + 1; /* +1 for 0-index array */
+  if (nextNum > SLIDES.length) {
+    nextNum = 1;
+  }
+  showSlide(nextNum);
+}
+
+function prevSlide() {
+  let prevNum = SLIDES.index($(".slide:not(.hidden)")) - 1 + 1; /* +1 for 0-index array */
+  if (prevNum <= 0) {
+    prevNum = SLIDES.length;
+  }
+  showSlide(prevNum);
+}
+
+function showSlide(num) {
+  let index = num - 1;
+  let currentSlide = SLIDES.eq(index);
+
+  /* TODO: snippets to hide SLIDES and then show currentSlide */
+  SLIDES.addClass("hidden");
+  currentSlide.removeClass("hidden");
+
+  const DOTS = $(".dots");
+  let currentDot = DOTS.eq(index);
+  DOTS.removeClass("dot_color");
+  currentDot.addClass("dot_color");
+
+
+
+}
+$("#dot1").click(function() {
+  showSlide(1);
+});
+$("#dot2").click(function() {
+  showSlide(2);
+});
+$("#dot3").click(function() {
+  showSlide(3);
+});
+
+$("#left_button").click(function() {
+  console.log("left button clicked!");
+  prevSlide();
+});
+
+$("#right_button").click(function() {
+  console.log("right button clicked!");
+  nextSlide();
+});
